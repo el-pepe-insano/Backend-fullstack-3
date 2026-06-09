f0d7778b7448307d224fbd1bf395285110b9d6a5
Acer <di.laraa@duocuc.cl>
2026-06-08 22:16:55 -0400

    mejora de seguridad :3

---
+++ Backend-fullstack-3-main/Pedidos/src/main/java/com/GodOfGames/Pedidos/services/PedidoService.java
@@ -0,0 +1,16 @@
+package com.GodOfGames.Pedidos.services;
+
+import com.GodOfGames.Pedidos.dtos.PedidoRequestDTO;
+import com.GodOfGames.Pedidos.dtos.PedidoResponseDTO;
+import com.GodOfGames.Pedidos.models.EstadoPedido;
+
+import java.util.List;
+
+public interface PedidoService {
+    
+    PedidoResponseDTO crearPedido(PedidoRequestDTO pedidoDTO, String usuarioId, String token);
+    PedidoResponseDTO obtenerPedidoPorId(Long id);
+    List<PedidoResponseDTO> obtenerTodosLosPedidos();
+    PedidoResponseDTO actualizarEstado(Long id, EstadoPedido nuevoEstado, String token);
+    List<PedidoResponseDTO> obtenerPedidosPorUsuario(String usuarioId);
+}
