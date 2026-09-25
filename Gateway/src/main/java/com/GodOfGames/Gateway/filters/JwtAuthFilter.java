55c27d56c422d47b683ca577c70dbcacea8fece7
Acer <di.laraa@duocuc.cl>
2026-09-25 20:39:12 -0300

    fix: elimina bean JWT duplicado, corrige ruta publica que tapaba /admin, agrega config de test

--- Gateway/src/main/java/com/GodOfGames/Gateway/filters/JwtAuthFilter.java
+++ Gateway/src/main/java/com/GodOfGames/Gateway/filters/JwtAuthFilter.java
@@ -1,4 +1,4 @@
-﻿package com.GodOfGames.Gateway.filters;
+package com.GodOfGames.Gateway.filters;
 
 import org.springframework.cloud.gateway.filter.GatewayFilterChain;
 import org.springframework.cloud.gateway.filter.GlobalFilter;
